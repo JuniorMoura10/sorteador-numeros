eccd49e4e24cd5223c2715dc292a0745b4476831
Júnior Moura <geraldomartins10@gmail.com>
2025-10-22 10:01:19 -0300

    Implementação da validação em caso do usuário não digitar os números.

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
+/**
+ * O programa sorteia números aleatórios, onde o usuário pode definir a quantidade de números a serem sorteados
+ * e em qual intervalo de números ele deseja obtê-los. 
+ */
+//declaração da função principal (sortear) e variáveis
 function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
 
     // Emite um alerta caso os campos não sejam preenchidos
-    if(quantidade == '' || de == '' || ate == ''){
+    if(isNaN(quantidade) || isNaN(de) || isNaN(ate)){
 
         alert('Favor, preencher os campos abaixo!');
         return;       
     }
 
+    // Array para guardar os números sorteados
     let sorteados = [];
+    // número efetivamente sorteado
     let numero;
 
      
 
-
+    /**
+     * Esta validação confere se o número inicial é menor que o número final. Em caso negatico, o programa emite um alerta ao 
+     * usuário e solicita a correção. Em caso positivo, o programa executa normalmente, fazendo o sorteio dos números.
+     */
     if(de > ate){
 
          alert('O número inicial deve ser menor que o número final!');
@@ -29,11 +39,13 @@ function sortear(){
             numero = obterNumerosAleatorios(de, ate);
         }
 
-        sorteados.push(numero);
-
-        alterarStatusBotao();
+        sorteados.push(numero);      
        
     }
+
+    //Chamada da função que ativa o botão reiniciar após a execução do programa
+     alterarStatusBotao();
+    // Este trecho do código trabalha a frase que apresenta os números sorteado
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
     }    
@@ -41,7 +53,7 @@ function sortear(){
 }
 
 
-
+//Declaração das demais funções
 function obterNumerosAleatorios(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
